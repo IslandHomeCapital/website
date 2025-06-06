dffbda6c14045243e5d47702460bd63bc8390c29
Brett Evans <36932+brettevans@users.noreply.github.com>
2025-06-06 16:33:52 +0000

    Remove `List`

--- Sources/main.swift
+++ Sources/main.swift
@@ -20,14 +20,9 @@ struct TestimonialMetadata : Metadata, Decodable {
     let relationship: Relationship
 }
 
-struct ListItem : Decodable {
-    let stringValue: String
-    let intValue: Int?
-}
-
 struct HomeMetadata : Metadata {
     let hero: String 
-    let testimonials: [ListItem]
+    let testimonials: String
 }
 
 struct PageMetadata : Metadata {
@@ -191,7 +186,7 @@ func renderHome(context: ItemRenderingContext<HomeMetadata>) -> Node {
             }
         }
         p {
-            context.item.metadata.testimonials.map { $0.stringValue }.joined()
+            context.item.metadata.testimonials.joined()
         }
     }                    
 }
